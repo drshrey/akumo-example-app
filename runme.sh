c8367421c1f3d06a32fc50a7e44f272ad195adf6
Shreyas <shreyas.jaganmohan@gmail.com>
2018-12-16 14:52:05 -0800

    push

---
+++ runme.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# test docker builder
+
+r=$(( $RANDOM % 255 ))
+g=$(( $RANDOM % 255 ))
+b=$(( $RANDOM % 255 ))
+
+cat << EOF > /var/www/html/index.html
+<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome to ${DEPLOYTEST}</title>
+<style>
+.Main {
+  margin: auto;
+  text-align: center;
+  font-family: Arial;
+}
+</style>
+</head>
+<body>
+<div class="Main">
+<h1>Welcome to Kumo - ${DEPLOYTEST}!</h1>
+</div>
+</body>
+</html>
+EOF
+
+if [[ -z "$ENV_TEST" ]]; then 
+	echo "Exiting due to lack of environment variable: ENV_TEST" > /dev/stderr
+	exit 123
+fi
+
+if [[ ! -f /etc/secrets/secrets-test.txt ]]; then
+	echo "Exiting due to lack of secrets file: /etc/secrets/secrets-test.txt" > /dev/stderr
+	exit 123
+fi
+
+exec /usr/sbin/nginx -g "daemon off;"
